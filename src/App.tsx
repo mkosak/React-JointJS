96dff4189c28c6be048f13bd264b12a8533f1829
Maksym Kosak <maksym.kosak@flixbus.com>
2021-02-03 06:19:33 +0200

    create app and running

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="canvas">It works!</div>
+    );
+  }
+}
+
+export default App;
